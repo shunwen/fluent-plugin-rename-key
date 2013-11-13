2f05b424f7bdad97b1bb97e1f107638bcd2a6dc1
shunwen <hsiaoshunwen@gmail.com>
2013-11-13 21:36:25 +0800

    Refactored and added some features

--- lib/fluent/plugin/out_rename_key.rb
+++ lib/fluent/plugin/out_rename_key.rb
@@ -2,54 +2,69 @@ class Fluent::RenameKeyOutput < Fluent::Output
   Fluent::Plugin.register_output 'rename-key', self
 
   config_param :remove_tag_prefix, :string, default: nil
+  config_param :append_tag, :string, default: 'key_renamed'
+  config_param :deep_rename, :bool, default: true
 
   def configure conf
     super
 
     @rename_rules = []
-
     conf_rename_rules = conf.keys.select { |k| k =~ /^rename_rule(\d+)$/ }
     conf_rename_rules.sort_by { |r| r.sub('rename_rule', '').to_i }.each do |r|
-      key_regexp, new_key, new_tag = parse_rename_rule conf[r]
+      key_regexp, new_key = parse_rename_rule conf[r]
 
-      if key_regexp.nil? || new_key.nil? || new_tag.nil?
-        raise Fluent::ConfigError, "Failed to parse[#{key_regexp},#{new_key},#{new_tag}]: #{r} #{conf[r]}"
-      end
-
-      if new_tag.match /\$\{tags\[\d\.\.\.?\d\]\}/
-        raise Fluent::ConfigError, "${tag} does not support range: #{r} #{conf[r]}"
+      if key_regexp.nil? || new_key.nil?
+        raise Fluent::ConfigError, "Failed to parse[#{key_regexp},#{new_key}]: #{r} #{conf[r]}"
       end
 
       if @rename_rules.map { |e| e.first }.include? /#{key_regexp}/
         raise Fluent::ConfigError, "Duplicated rules for key #{key_regexp}: #{@rename_rules}"
       end
 
-      @rename_rules << [/#{key_regexp}/, new_key, new_tag]
+      @rename_rules << [/#{key_regexp}/, new_key]
       $log.info "Added rename key rule: #{r} #{@rename_rules.last}"
     end
 
     raise Fluent::ConfigError, "No rename rules are given" if @rename_rules.empty?
-    @remove_tag_prefix = /^#{Regexp.escape @remove_tag_prefix}/ if @remove_tag_prefix
+
+    @remove_tag_prefix = /^#{Regexp.escape @remove_tag_prefix}\.?/ if @remove_tag_prefix
   end
 
   def emit tag, es, chain
     es.each do |time, record|
-      new_tag = retag tag, record
-      next if new_tag.nil? || new_tag == tag
-      Fluent::Engine.emit new_tag, time, record
+      new_tag = @remove_tag_prefix ? tag.sub(@remove_tag_prefix, '') : tag
+      new_tag = "#{new_tag}.#{@append_tag}".sub(/^\./, '')
+      new_record = rename_key record
+      Fluent::Engine.emit new_tag, time, new_record
     end
+
+    chain.next
   end
 
   # private
 
   def parse_rename_rule rule
-    if rule.match /^([^\s]+)\s+([^\s]+)\s+([^\s]+)$/
+    if rule.match /^([^\s]+)\s+([^\s]+)$/
       return $~.captures
     end
   end
 
-  def retag tag, record
+  def rename_key record
+    new_record = {}
 
+    record.each do |key, value|
+
+      @rename_rules.each do |rule|
+        match_data = key.match rule[0]
+        next unless match_data
+        key = 'x$url'
+        break
+      end
+
+      new_record[key] = value
+    end
+
+    new_record
   end
 
   def filter_key_starts_with_dollar_sign original_params
--- spec/plugin/out_rename_key_spec.rb
+++ spec/plugin/out_rename_key_spec.rb
@@ -7,31 +7,32 @@ describe Fluent::RenameKeyOutput do
   end
 
   CONFIG = %q[
-    rename_rule1 ^\$(.+) ${captures[1]} ${tag}.new1
-    rename_rule2 ^[.]+([^.]+)[.\s]*([^.]+) ${captures[1]}\ssomthing\s${captures[2]} ${tag}.new2
+    rename_rule1 ^\$(.+) ${captures[1]}
+    rename_rule2 ^[.]+([^.]+)[.\s]*([^.]+) ${captures[1]}\ssomthing\s${captures[2]}
   ]
 
   CONFIG_MULTI_RULES_FOR_A_KEY = %q[
-    rename_rule1 ^\$(.+) ${captures[1]} new.tag
-    rename_rule2 ^\$(.+) ${captures[1]}\ssomthing new.tag
+    rename_rule1 ^\$(.+) ${captures[1]}
+    rename_rule2 ^\$(.+) ${captures[1]}\ssomthing
   ]
 
   CONFIG_REMOVE_TAG_PREFIX = %q[
-    rename_rule1 ^\$(.+) ${captures[1]}\ssomthing ${tag}.new
+    rename_rule1 ^\$(.+) ${captures[1]}\ssomthing
     remove_tag_prefix input
+  ]
+
+  CONFIG_APPEND_TAG = %q[
+    rename_rule1 ^\$(.+) ${captures[1]}\ssomthing
+    append_tag postfix
   ]
 
   def create_driver conf=CONFIG, tag='test'
     Fluent::Test::OutputTestDriver.new(Fluent::RenameKeyOutput, tag).configure(conf)
   end
 
-  context "#configure" do
+  context "configurations" do
     it "raises error when no configuration" do
       expect{create_driver ''}.to raise_error Fluent::ConfigError
-    end
-
-    it "raises error when rule doesn't provide new tag" do
-      expect{create_driver 'rename_rule1 ^$(.+?) ${captures[1]} '}.to raise_error Fluent::ConfigError
     end
 
     it "raises error when rule is incomplete" do
@@ -44,28 +45,44 @@ describe Fluent::RenameKeyOutput do
 
     it "configures multiple rules" do
       d = create_driver
-      expect(d.instance.config['rename_rule1']).to eq '^\$(.+) ${captures[1]} ${tag}.new1'
-      expect(d.instance.config['rename_rule2']).to eq '^[.]+([^.]+)[.\s]*([^.]+) ${captures[1]}\ssomthing\s${captures[2]} ${tag}.new2'
+      expect(d.instance.config['rename_rule1']).to eq '^\$(.+) ${captures[1]}'
+      expect(d.instance.config['rename_rule2']).to eq '^[.]+([^.]+)[.\s]*([^.]+) ${captures[1]}\ssomthing\s${captures[2]}'
     end
   end
 
-  xit "removes tag prefix" do
-    d1 = create_driver CONFIG_REMOVE_TAG_PREFIX, 'input.access'
-    d1.run do
-      d1.emit '$url' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000
+  context "emits" do
+    it "removes tag prefix" do
+      d = create_driver CONFIG_REMOVE_TAG_PREFIX, 'input.test'
+      d.run { d.emit 'test' => 'data' }
+      expect(d.emits[0][0]).not_to start_with 'input'
     end
 
-    emits = d1.emits
-    emits.should have(1).items
+    it "appends additional tag" do
+      d = create_driver CONFIG_APPEND_TAG, 'input.test'
+      d.run { d.emit 'test' => 'data' }
+      expect(d.emits[0][0]).to eq 'input.test.postfix'
+    end
   end
 
   context "private methods" do
     describe "#parse_rename_rule" do
-      let(:rename_rule_example) { '^\$(.+) #{captures[1]} new.tag' }
+      let(:rename_rule_example) { '^\$(.+) #{captures[1]}' }
       let(:result) { Fluent::RenameKeyOutput.new.parse_rename_rule rename_rule_example }
 
-      it "captures 3 items" do
-        expect(result).to have(3).items
+      it "captures 2 items, the key_regexp and new_name" do
+        expect(result).to have(2).items
+      end
+    end
+
+    describe "#rename_key" do
+      it "replace key name which matches the key_regexp at the first level" do
+        d = create_driver 'rename_rule1 ^\$(.+) x$${captures[1]}'
+        d.run do
+          d.emit '$url' => 'www.google.com', 'level2' => {'$1' => 'options1'}
+        end
+        emits = d.emits
+        expect(emits).to have(1).items
+        expect(emits[0][2]).to have_key 'x$url'
       end
     end
   end
