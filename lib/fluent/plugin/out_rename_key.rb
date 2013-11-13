6f0c4b119a7af75345b785ca5d0c2d923890d456
shunwen <hsiaoshunwen@gmail.com>
2013-11-13 19:46:36 +0800

    Handles configuration and detects exceptions

--- lib/fluent/plugin/out_rename_key.rb
+++ lib/fluent/plugin/out_rename_key.rb
@@ -3,7 +3,63 @@ class Fluent::RenameKeyOutput < Fluent::Output
 
   config_param :remove_tag_prefix, :string, default: nil
 
-  MATCH_OPERATOR_EXCLUDE = '!'
+  def configure conf
+    super
 
+    @rename_rules = []
 
+    conf_rename_rules = conf.keys.select { |k| k =~ /^rename_rule(\d+)$/ }
+    conf_rename_rules.sort_by { |r| r.sub('rename_rule', '').to_i }.each do |r|
+      key_regexp, new_key, new_tag = parse_rename_rule conf[r]
+
+      if key_regexp.nil? || new_key.nil? || new_tag.nil?
+        raise Fluent::ConfigError, "Failed to parse[#{key_regexp},#{new_key},#{new_tag}]: #{r} #{conf[r]}"
+      end
+
+      if new_tag.match /\$\{tags\[\d\.\.\.?\d\]\}/
+        raise Fluent::ConfigError, "${tag} does not support range: #{r} #{conf[r]}"
+      end
+
+      if @rename_rules.map { |e| e.first }.include? /#{key_regexp}/
+        raise Fluent::ConfigError, "Duplicated rules for key #{key_regexp}: #{@rename_rules}"
+      end
+
+      @rename_rules << [/#{key_regexp}/, new_key, new_tag]
+      $log.info "Added rename key rule: #{r} #{@rename_rules.last}"
+    end
+
+    raise Fluent::ConfigError, "No rename rules are given" if @rename_rules.empty?
+    @remove_tag_prefix = /^#{Regexp.escape @remove_tag_prefix}/ if @remove_tag_prefix
+  end
+
+  def emit tag, es, chain
+    es.each do |time, record|
+      new_tag = retag tag, record
+      next if new_tag.nil? || new_tag == tag
+      Fluent::Engine.emit new_tag, time, record
+    end
+  end
+
+  # private
+
+  def parse_rename_rule rule
+    if rule.match /^([^\s]+)\s+([^\s]+)\s+([^\s]+)$/
+      return $~.captures
+    end
+  end
+
+  def retag tag, record
+
+  end
+
+  def filter_key_starts_with_dollar_sign original_params
+    filtered_params = {}
+    original_params.each do |key, value|
+      key = 'x$' + key[1..-1] if key.start_with? '$'
+      value = filter_key_starts_with_dollar_sign value if value.is_a? Hash
+      filtered_params[key] = value
+    end
+
+    filtered_params
+  end
 end
--- spec/plugin/out_rename_key_spec.rb
+++ spec/plugin/out_rename_key_spec.rb
@@ -2,6 +2,72 @@ require 'spec_helper'
 require 'fluent/plugin/out_rename_key'
 
 describe Fluent::RenameKeyOutput do
-  it "" do
+  before :each do
+    Fluent::Test.setup
   end
+
+  CONFIG = %q[
+    rename_rule1 ^\$(.+) ${captures[1]} ${tag}.new1
+    rename_rule2 ^[.]+([^.]+)[.\s]*([^.]+) ${captures[1]}\ssomthing\s${captures[2]} ${tag}.new2
+  ]
+
+  CONFIG_MULTI_RULES_FOR_A_KEY = %q[
+    rename_rule1 ^\$(.+) ${captures[1]} new.tag
+    rename_rule2 ^\$(.+) ${captures[1]}\ssomthing new.tag
+  ]
+
+  CONFIG_REMOVE_TAG_PREFIX = %q[
+    rename_rule1 ^\$(.+) ${captures[1]}\ssomthing ${tag}.new
+    remove_tag_prefix input
+  ]
+
+  def create_driver conf=CONFIG, tag='test'
+    Fluent::Test::OutputTestDriver.new(Fluent::RenameKeyOutput, tag).configure(conf)
+  end
+
+  context "#configure" do
+    it "raises error when no configuration" do
+      expect{create_driver ''}.to raise_error Fluent::ConfigError
+    end
+
+    it "raises error when rule doesn't provide new tag" do
+      expect{create_driver 'rename_rule1 ^$(.+?) ${captures[1]} '}.to raise_error Fluent::ConfigError
+    end
+
+    it "raises error when rule is incomplete" do
+      expect{create_driver 'rename_rule1 ^$(.+?) '}.to raise_error Fluent::ConfigError
+    end
+
+    it "raises error when multiple rules are set for the same key pattern" do
+      expect{create_driver CONFIG_MULTI_RULES_FOR_A_KEY}.to raise_error Fluent::ConfigError
+    end
+
+    it "configures multiple rules" do
+      d = create_driver
+      expect(d.instance.config['rename_rule1']).to eq '^\$(.+) ${captures[1]} ${tag}.new1'
+      expect(d.instance.config['rename_rule2']).to eq '^[.]+([^.]+)[.\s]*([^.]+) ${captures[1]}\ssomthing\s${captures[2]} ${tag}.new2'
+    end
+  end
+
+  xit "removes tag prefix" do
+    d1 = create_driver CONFIG_REMOVE_TAG_PREFIX, 'input.access'
+    d1.run do
+      d1.emit '$url' => 'www.google.com', 'path' => '/foo/bar?key=value', 'agent' => 'Googlebot', 'response_time' => 1000000
+    end
+
+    emits = d1.emits
+    emits.should have(1).items
+  end
+
+  context "private methods" do
+    describe "#parse_rename_rule" do
+      let(:rename_rule_example) { '^\$(.+) #{captures[1]} new.tag' }
+      let(:result) { Fluent::RenameKeyOutput.new.parse_rename_rule rename_rule_example }
+
+      it "captures 3 items" do
+        expect(result).to have(3).items
+      end
+    end
+  end
+
 end
