5a540bc5f80e86e98902ef40e39d765c75f8ea32
shunwen <hsiaoshunwen@gmail.com>
2013-11-14 17:21:44 +0800

    Clean ARGV before requiring fluent/test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
 require 'rspec'
+
+ARGV = []
 require 'fluent/test'
 
 unless ENV.has_key?('VERBOSE')
